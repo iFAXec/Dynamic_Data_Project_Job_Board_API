0326f39515c58a66df5ba89e024b2f424832f5f6
ifaxec <xecfahim.ahmed@gmail.com>
2024-07-21 22:02:27 +0100

    Updated app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         let data = JSON.parse(this.responseText);
+        console.log(data);
         const jobs = document.querySelector("#container");
 
         data.slice(1).forEach((item, i) => {
@@ -10,13 +11,21 @@ xhttp.onreadystatechange = function () {
 
             div.innerHTML = `
 
-        
-
-            
-            
+        <div class = 'left-section'>
             <h5 class= 'company' >${item.company}</h5>
             <h6 class = 'position' >${item.position}</h6>
             <p class = 'location'>${item.location}</p>
+        </div>
+        <div class = 'mid-section'>
+            ${item.tags.map(tag =>
+                `<span class='tag'>${tag}</span>`
+            ).join(' ')}
+
+        <div class='right-section'>
+            <button class = 'apply'>Apply</button>
+        </div>
+
+        </div>
         `
             jobs.appendChild(div);
         });
